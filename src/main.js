20b72b8e3cd4663559cf8d3e016da2bd8bc144c3
MarioGarcia0211 <jhonmariogarciaballesteros@gmail.com>
2025-05-13 22:01:06 -0500

    Estructura inicial del proyecto ChallengeHub

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import "./style.css";
+import router from "./router";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "sweetalert2/dist/sweetalert2.min.css";
+
+createApp(App).use(router).mount("#app");
