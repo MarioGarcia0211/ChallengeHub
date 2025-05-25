dfd2a4e9d47106d5857fb3806d487b9275fa5d8d
MarioGarcia0211 <jhonmariogarciaballesteros@gmail.com>
2025-05-25 01:36:42 -0500

    Se implemento el color de fondo dependiendo de la ruta

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,26 @@
 
 <script>
 export default {
-  name: "App",
+  watch: {
+    $route(to) {
+      this.setBodyClass(to.path);
+    },
+  },
+  mounted() {
+    this.setBodyClass(this.$route.path);
+  },
+  methods: {
+    setBodyClass(path) {
+      document.body.className = "";
+
+      if (path === "/login" || path === "/register") {
+        document.body.classList.add("bg-login");
+      } else if (path === "/") {
+        document.body.classList.add("bg-default");
+      } else {
+        document.body.classList.add("bg-default");
+      }
+    },
+  },
 };
 </script>
